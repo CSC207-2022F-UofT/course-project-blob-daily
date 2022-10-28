af81784764359aff20c8357d69359c773e253f33
hy2308 <113389504+hy2308@users.noreply.github.com>
2022-10-27 21:57:49 -0400

    Add files via upload

---
+++ src/backend/entites/FeedItem.java
@@ -0,0 +1,16 @@
+
+public class FeedItem{
+
+    //Attributes
+    private ProtectedAccount account;
+    private Avatar avatar;
+    private TaskCompletionRecord record;
+
+    // Constructor
+     public FeedItem(ProtectedAccount acccount, TaskCompletionRecord record, Avatar avatar){
+        this.account = acccount;
+        this.record = record;
+        this.avatar = avatar;
+    }
+}
+
