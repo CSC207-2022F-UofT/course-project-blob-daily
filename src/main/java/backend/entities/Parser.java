59c0c61a772ed6e00a9221ef2b33301eec7a5855
Wo Ming Boaz Cheung <92407344+rethegreat@users.noreply.github.com>
2022-11-14 21:42:02 -0500

    added parser and shop manager test cases

---
+++ src/main/java/backend/entities/Parser.java
@@ -0,0 +1,10 @@
+package backend.entities;
+
+import java.util.HashMap;
+
+public interface Parser {
+
+    public static HashMap convert(String stringObject) {
+        return null;
+    }
+}
--- src/main/java/backend/useCases/shopManager.java
+++ src/main/java/backend/useCases/shopManager.java
@@ -2,41 +2,49 @@ package backend.useCases;
 
 import backend.entities.IDs.AccountID;
 import backend.entities.IDs.ItemID;
+import backend.entities.Parser;
 import backend.entities.Pet;
 import backend.entities.ShopItem;
+import backend.error.handlers.LogHandler;
 
+import java.io.IOException;
 import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
+import java.util.Locale;
+import java.util.stream.Stream;
 
 
-public class shopManager {
+public class shopManager implements Parser {
 
 //    Not yet implemented verify session
 
-
-    public HashMap hashmapConverter(String stringObject){
+    public static HashMap convert(String stringObject){
         HashMap<String, Object> hashObject = new HashMap<>();
         int keyIndex = 1;
         int colonIndex = stringObject.indexOf(":");
         int commaIndex = stringObject.indexOf(",");
+
+        //If comma doesn't exist in the stringObject, set it to be last index
         if(commaIndex == -1){
             commaIndex = stringObject.length() - 1;
         }
+
         int listIndex = stringObject.indexOf("[");
+
+
         while (stringObject.length() > 1){
             String key = stringObject.substring(keyIndex, colonIndex);
-            System.out.print(key);
-            if(listIndex != -1 && listIndex < commaIndex){
-                ArrayList<String> value = new ArrayList<>();
-                hashObject.put(key, value);
-            }
-            else {
-                String value = stringObject.substring(colonIndex + 2, commaIndex);
-                hashObject.put(key, value);
-                System.out.println(value);
-            }
+
+            // If "[" exist in stringObject, then there is a list in the hashmap
+
+            String value = stringObject.substring(colonIndex + 2, commaIndex);
+            hashObject.put(key, value);
+
+
+
+            // updating stringObject to not included the previously added key-value pair
             if(commaIndex + 2 > stringObject.length()){
                 stringObject = "";
             }else{
@@ -54,11 +62,11 @@ public class shopManager {
     }
 
 
-    public List<ShopItem> getShopItems(){
-        ArrayList<ShopItem> shopItems= new ArrayList<>();
-        ArrayList<String> itemList= new ArrayList<>(List.of("{itemID: 12jadf2, cost: 13.2, name: pants, description: it's very comfortable, image: abc}")); //dataBase.getShopItems;
+    public static List<ShopItem> getShopItems() {
+        ArrayList<ShopItem> shopItems = new ArrayList<>();
+        ArrayList<String> itemList = new ArrayList<>(List.of("{itemID: 12jadf2, cost: 13.2, name: pants, description: it's very comfortable, image: abc}")); //dataBase.getShopItems;
         for (String s : itemList) {
-            HashMap hashObject = hashmapConverter(s);
+            HashMap hashObject = convert(s);
             String itemIDString = (String) hashObject.get("itemID");
             Double cost = Double.valueOf((String) hashObject.get("cost"));
             String name = (String) hashObject.get("name");
@@ -69,39 +77,38 @@ public class shopManager {
             ItemID itemID = new ItemID(itemIDString);
 
 
-
             shopItems.add(new ShopItem(itemID, cost, name, description, imageString));
         }
         return shopItems;
 
     }
 
-    public Pet getPet(String sessionID){
+    public static Pet getPet(String sessionID){
         String petString = "{accountID: 12jadf2, health: 13.2, inventory: [pants], currentOutfit: [pants]}"; //dataBase.getPet;
-        HashMap petHash = hashmapConverter(petString);
+        HashMap petHash = convert(petString);
 
         AccountID accountID = new AccountID((String) petHash.get("accountID"));
-        Double health = (Double) petHash.get("health");
-        ArrayList<ShopItem> inventory = (ArrayList<ShopItem>) petHash.get("inventory");
-        ArrayList<ShopItem> currentOutfit = (ArrayList<ShopItem>) petHash.get("currentOutfit");
+        Double health = Double.parseDouble((String) petHash.get("health"));
+        ArrayList<ShopItem> inventory = (ArrayList) petHash.get("inventory");
+        ArrayList<ShopItem> currentOutfit = (ArrayList) petHash.get("currentOutfit");
 
         Pet pet = new Pet(accountID, health, inventory, currentOutfit);
 
         return pet;
     }
 
-    public boolean updateCurrentOutfit(String sessionID, List<String> newOutfit){
+    public static boolean updateCurrentOutfit(String sessionID, List<String> newOutfit){
         throw new UnsupportedOperationException();
     }
 
-    public double getBalance(String sessionID){
+    public static double getBalance(String sessionID){
         String balanceString = "{balance: 28.9}"; //dataBase.getPet;
-        HashMap balanceHash = hashmapConverter(balanceString);
-        Double balance = (Double) balanceHash.get("balance");
-        return balance;
+        HashMap balanceHash = convert(balanceString);
+        return Double.parseDouble((String) balanceHash.get("balance"));
     }
 
-    public boolean purchaseItem(String itemID, String sessionID) {
+    public static boolean purchaseItem(String itemID, String sessionID) {
         throw new UnsupportedOperationException();
     }
+
 }
