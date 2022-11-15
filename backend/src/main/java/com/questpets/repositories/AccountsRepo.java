92bdaf6e401cb269fb28a19630b2017b6b72b6c7
Tony Kim <95389617+TonyKim02@users.noreply.github.com>
2022-11-15 18:56:53 -0500

    Delete maven based backend directory
    This directory has no use since gralde ver has been successfully established

--- backend/src/main/java/com/questpets/repositories/AccountsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.questpets.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AccountsRepo extends MongoRepository<com.questpets.entities.users.DBAccount, String> {
-    com.questpets.entities.users.DBAccount findByAccountID(String accountID);
-    com.questpets.entities.users.DBAccount findBySessionID(String sessionID);
-    com.questpets.entities.users.DBAccount findByUsername(String username);
-}
