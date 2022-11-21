3044a37e99c211947990855f3842c027b705e47b
ShaanPurewal <89044277+ShaanPurewal@users.noreply.github.com>
2022-11-20 22:07:56 -0500

    Implement additional logging and response handling features

---
+++ src/test/java/usecases/AccountManagerTest.java
@@ -0,0 +1,20 @@
+package usecases;
+
+import com.backend.QuestPetsApplication;
+import com.backend.usecases.AccountManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = QuestPetsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AccountManagerTest {
+
+    @Autowired
+    private AccountManager accountManager;
+
+    @Test
+    public void testLoginValid(){
+        Assertions.assertTrue(true);
+    }
+}
