94358f6e9eef234014e064fbd10e570707bcc23a
Wo Ming Boaz Cheung <92407344+rethegreat@users.noreply.github.com>
2022-11-20 19:44:18 -0500

    updated import statement

--- src/main/java/com/backend/repositories/PetRepo.java
+++ src/main/java/com/backend/repositories/PetRepo.java
@@ -2,7 +2,7 @@ package com.backend.repositories;
 
 import com.backend.entities.Pet;
 import com.backend.entities.ShopItem;
-import com.backend.entities.users.DBAccount;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
--- src/main/java/com/backend/repositories/ShopItemRepo.java
+++ src/main/java/com/backend/repositories/ShopItemRepo.java
@@ -1,7 +1,7 @@
 package com.backend.repositories;
 
 import com.backend.entities.ShopItem;
-import com.backend.entities.users.DBAccount;
+
 import com.backend.entities.ShopItem;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
